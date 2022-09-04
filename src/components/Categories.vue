9f262916085a793aa2419131284dd62fedbbc480
AHTepsilon <alehertoro725@gmail.com>
2022-09-04 14:03:09 -0500

    fixed up the categories page

--- src/components/Categories.vue
+++ src/components/Categories.vue
@@ -1,7 +1,30 @@
 <template>
     <section>
-        <h1>Categories</h1>
-        <p>Search throughout all products by category.</p>
+		<div class="section_text">
+			<h1 class="section_text_title">Categories</h1>
+			<p class="section_text_paragraph">Search throughout all products by category</p>	
+		</div>
+		
+		<div class="section_categories">
+			<div class="section_categories_item">
+				<h3 class="section_categories_item_title">Chemicals</h3>
+			</div>
+			<div class="section_categories_item">
+				<h3 class="section_categories_item_title">Lab Equimpent</h3>
+			</div>
+			<div class="section_categories_item">
+				<h3 class="section_categories_item_title">Glassware</h3>
+			</div>
+			<div class="section_categories_item">
+				<h3 class="section_categories_item_title">Safety Equipment</h3>
+			</div>
+			<div class="section_categories_item">
+				<h3 class="section_categories_item_title">Books</h3>
+			</div>
+			<div class="section_categories_item">
+				<h3 class="section_categories_item_title">Biological Equipment</h3>
+			</div>
+		</div>
     </section>
 </template>
 
@@ -9,6 +32,62 @@
 
 </script>
 
-<style>
+<style lang="scss">
+	$headerColor: #FFFFFF;
+    $babyBlue: #DAE4FF;
+    $darkCyan: #A6BFFF;
+	
+	section
+	{
+		.section_text
+		{
+			
+		}
+		
+		.section_categories
+		{
+			
+			display: flex;
+			flex-direction: row;
+			flex-wrap: wrap;
+			
+			align-items: center;
+			justify-content: center;
+			
+			&_item
+			{
+				width: 100px;
+				height: 200px;
+				
+				background: $babyBlue;
+				margin: 5em;
+				
+				align-items: center;
+				justify-content: center;
+				
+				text-align: center;
+				
+				padding: 30px;
+				top: 0;
+				
+				
+				&_title
+				{
+
+				}
+				
+				transition: transform .2s;
+				
+				&:hover
+				{
+					cursor: pointer;
+					background: darken($color: $babyBlue, $amount: 5);
+					
+					transform: scale(1.2);
+				}
+				
+			}
+		}
+	}
 
 </style>
--- src/components/Landing.vue
+++ src/components/Landing.vue
@@ -1,7 +1,13 @@
 <template>
 
 	<section>
-		<h1>Chemistry Supplies for Schools and Students</h1>
+		<div class="section_text">
+			<h1 class="section_text_title">Chemistry Supplies for Schools and Students</h1>
+			<p class="section_text_paragraph">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+			Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+			Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat </p>
+			<button>Sign Up Now</button>
+		</div>
     </section>
     
 </template>
@@ -11,5 +17,13 @@
 </script>
 
 <style lang="scss">
-
+	section
+	{
+		margin: 4em;
+		
+		.section_text
+		{
+			margin-right: 30em;
+		}
+	}
 </style>
