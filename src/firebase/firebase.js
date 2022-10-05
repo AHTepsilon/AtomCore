a999d398d3fe824acffc74452b7d2411b413b432
Alejandro Hernandez Toro <1006009372@u.icesi.edu.co>
2022-10-04 19:21:21 -0500

    initialized firebase

---
+++ src/firebase/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBpNQJHBdZV29lrgcXEnd3xBKsm5Tr0u08",
+  authDomain: "atomcore-7f735.firebaseapp.com",
+  projectId: "atomcore-7f735",
+  storageBucket: "atomcore-7f735.appspot.com",
+  messagingSenderId: "902204108279",
+  appId: "1:902204108279:web:a7904f5a18957339e252f2"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
